e4bb358044ae2091fc4df07c76f19a2b3e311d0d
Asta Adukaite <asta.adukaite@ef.com>
2020-04-16 18:27:10 +0200

    first commit

---
+++ firstjavaproject/src/days/first/SecondTask.java
@@ -0,0 +1,27 @@
+package days.first;
+
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        System.out.println("Iveskite polindroma");
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine().replaceAll(" ", "");
+        if(isPalindrome(word)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("word is not a palindrome");
+        }
+
+    }
+
+    private static boolean isPalindrome(String word) {
+        for(int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) != word.charAt((word.length() - 1 -i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
