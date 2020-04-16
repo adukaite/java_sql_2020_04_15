035bb2ae099d3b7e3e2dd7df68104632e463ba90
adukaite <asta.adukaite@ef.com>
2020-04-16 18:37:09 +0200

    add new line

--- firstjavaproject/src/days/first/SecondTask.java
+++ firstjavaproject/src/days/first/SecondTask.java
@@ -20,6 +20,7 @@ public class SecondTask {
         for(int i = 0; i < word.length(); i++) {
             if(word.charAt(i) != word.charAt((word.length() - 1 -i))) {
                 return false;
+
             }
         }
         return true;
